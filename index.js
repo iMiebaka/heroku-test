aca87e96f6c17dbe05764f42cd5c4a81c456b3ec
imiebaka <triple07even@gmail.com>
2022-10-18 00:21:53 +0100

    update multiple fixes

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ app.use(express.json())
 const PORT = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-res.json({"message": "the app is working"})})
-
+res.json({"message": "the api is working"})})
 
 
 app.listen(PORT, () => {

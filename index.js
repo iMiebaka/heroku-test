9f3c64d3e503c9652810918ce5334fd982bb03a8
imiebaka <triple07even@gmail.com>
2022-10-18 10:19:55 +0100

    installing ejs

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ console.log(process.env.DATABASE_URL)
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.set("view engine", "ejs")
 
 const PORT = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
   res.json({message: "the api is working. checking auto deploy"})
+})
+
+app.get("/page", (req, res) => {
+ res.render("index")
 })
 
 
